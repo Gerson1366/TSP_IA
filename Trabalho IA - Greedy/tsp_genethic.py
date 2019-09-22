3a997f228a6adc089feb2ed94e5a182a88a10a43
Gerson1366 <gersoncarneiro@msn.com>
2019-09-22 20:39:46 -0300

    Add files via upload

---
+++ Trabalho IA - Greedy/tsp_genethic.py
@@ -0,0 +1,322 @@
+from argparse import ArgumentParser
+import os.path
+import random
+from graph import Graph
+
+def intersection(list1, list2):
+    #intercessao entre duas listas
+    return list(set(list1).intersection(set(list2)))
+
+
+def swap(a, b, l):
+    #troque um elemento de indice a por um elemento de indice b em uma lista l   
+    l[b], l[a] = l[a], l[b]
+    return l
+
+
+class Individual:
+    '''
+    Individual with a sequence of genes. Represents any possible solution/path
+    '''
+
+    def __init__(self, genes, graph):
+        self.genes = genes
+        self.graph = graph
+
+    def __eq__(self, other):
+        return self.fitness() == other.fitness()
+
+    def __lt__(self, other):
+        return self.fitness() < other.fitness()
+
+    def __repr__(self):
+        return str(self.genes)
+
+    def copy(self):
+        return self.__class__(list(self.genes), self.graph)
+
+    def crossover(self, other):
+        '''
+        Returns two children by crossovering with another individual.
+        It is based on indexes of common values between individuals.
+        '''
+        # prevent start, stop points from crossover
+        genes1 = self.genes[1:-1]
+        genes2 = other.genes[1:-1]
+
+        # cut point to slice genes1 and genes2
+        cut_point = random.randint(0, len(genes1) - 1)
+
+        # common values in first slices
+        common_values1 = intersection(genes1[:cut_point], genes2[:cut_point])
+
+        # crossover first slices
+        for value in common_values1:
+            from_index = genes1.index(value)
+            to_index = genes2.index(value)
+            swap(from_index, to_index, genes1)
+
+        # common values in second slices
+        common_values2 = intersection(genes1[cut_point:], genes2[cut_point:])
+
+        # crossover second slices
+        for value in common_values2:
+            from_index = genes2.index(value)
+            to_index = genes1.index(value)
+            swap(from_index, to_index, genes2)
+
+        # reinsert start and stop points after crossover
+        genes1 = [self.start()] + genes1 + [self.start()]
+        genes2 = [other.start()] + genes2 + [self.start()]
+
+        return Individual(genes1, self.graph), Individual(genes2, self.graph)
+
+    def fitness(self):
+        '''
+        Returns the fitness (path cost) of individual
+        '''
+        fitness = 0
+        chromosome_size = len(self.genes)
+        for j in range(chromosome_size):
+            if j < chromosome_size - 1:
+                from_gene = self.genes[j]
+                to_gene = self.genes[j + 1]
+                cost = self.graph.cost(from_gene, to_gene)
+                fitness += cost
+        return fitness
+
+    def mutation(self):
+        '''
+        Generates a mutation based on this individual
+        '''
+        new_genes = list(self.genes)
+        # prevent start and stop points from mutation
+        a = random.randint(1, len(new_genes) - 2)  # first index
+        b = random.randint(1, len(new_genes) - 2)  # second index
+        swap(a, b, new_genes)
+        return Individual(new_genes, self.graph)
+
+    def start(self):
+        '''
+        Start gene of the individual
+        '''
+        return self.genes[0]
+
+
+class Population:
+    '''
+    Group of individuals that represents possible solutions to the problem
+    '''
+    def __init__(self, graph):
+        self.graph = graph
+        self.individuals = []
+
+    def __repr__(self):
+        return str(self.individuals)
+
+    def fitness(self):
+        '''
+        Sum of all individual fitness
+        '''
+        fitness = 0
+        for individual in self.individuals:
+            fitness += individual.fitness()
+        return fitness
+
+    def append(self, individual):
+        '''
+        Appends a new individual to the population
+        '''
+        if isinstance(individual, list):
+            individual = Individual(individual, self.graph)
+            self.individuals.append(individual)
+        if isinstance(individual, Individual):
+            self.individuals.append(individual)
+
+    def best(self):
+        '''
+        Returns best individual of the population (with minimal fitness).
+        '''
+        return min(self.individuals)
+
+    def get(self, index):
+        '''
+        Returns an individual by its index
+        '''
+        return self.individuals[index]
+
+    def has(self, i):
+        if isinstance(i, list):  # i is a list of genes of an individual
+            for individual in self.individuals:
+                if i == individual.genes:
+                    return True
+        if isinstance(i, Individual):  # i is an individual
+            return i in self.individuals
+
+    def roulette_wheel_selection(self):
+        '''
+        Select an individual based on roulette wheel selection strategy
+        '''
+        cumulative_fitness = 0.0
+        total_fraction = self.fitness() * random.random()  # random [0.0, 1.0]
+        for individual in self.individuals:
+            cumulative_fitness += individual.fitness()
+            if cumulative_fitness >= total_fraction:
+                return individual
+
+    def size(self):
+        '''
+        Returns number of individuals
+        '''
+        return len(self.individuals)
+
+
+class Solution:
+    '''
+    Solution of shortest path problem using genetic algorithm
+    '''
+    def __init__(self, start, graph, crossover_rate=0.7,
+                 elitism_rate=0.1, population_size=100, max_stagnation=100,
+                 mutation_rate=0.05):
+        # start point for each individual in generation or population
+        self.start = start
+        # graph of edge distances
+        self.graph = graph
+        # probability of two individuals crossing in population
+        self.crossover_rate = crossover_rate
+        # percentage of individual elitism in population
+        self.elitism_rate = elitism_rate
+        # number of individuals in population
+        self.population_size = population_size
+        # max stagnation in a population and its descendents
+        self.max_stagnation = max_stagnation
+        # probability of individual mutation in population
+        self.mutation_rate = mutation_rate
+
+    def random_population(self, population_size=None):
+        '''
+        Generates a random population with a predefined size
+        '''
+        population_size = population_size or self.population_size
+        population = Population(self.graph)
+
+        while population.size() < population_size:
+            genes = self.random_genes()
+            if(not population.has(genes)):  # preventing twins
+                population.append(genes)
+
+        return population
+
+    def random_genes(self):
+        '''
+        Generate random genes (without repetions) for an individual.
+        '''
+        genes = []
+
+        vertices = self.graph.vertices()
+        missing_genes = list(vertices)  # copy
+        missing_genes.remove(self.start)
+
+        genes.append(self.start)
+
+        for i in range(len(missing_genes)):
+            gene = random.choice(missing_genes)  # choosing a distinct gene
+            genes.append(gene)
+            missing_genes.remove(gene)  # removing already choosed gene
+
+        genes.append(self.start)
+
+        return genes
+
+    def solve(self):
+        '''
+        Creates lazy generations (genetic algorithm) and returns the last one
+        '''
+
+        population = self.random_population(self.population_size)
+
+        # number of times there is no improvement
+        stagnation_count = 0
+
+        # number of generations created
+        generations_count = 0
+
+        while stagnation_count < self.max_stagnation:
+            new_population = Population(self.graph)
+
+            # elitism
+            individuals = list(population.individuals)
+            elitism_size = int(self.population_size * self.elitism_rate)
+            while new_population.size() < elitism_size:
+                best_individual = min(individuals)  # with min fitness
+                new_population.append(best_individual)
+                individuals.remove(best_individual)  # remove already computed
+
+            while new_population.size() < self.population_size:
+                father = population.roulette_wheel_selection()
+                mother = population.roulette_wheel_selection()
+
+                son = daugther = None
+
+                # crossover
+                if (random.random() < self.crossover_rate):
+                    son, daugther = mother.crossover(father)
+
+                # mutation
+                if (random.random() < self.mutation_rate):
+                    son = father.mutation()
+
+                if (random.random() < self.mutation_rate):
+                    daugther = mother.mutation()
+
+                # preventing None values
+                son = son or father.copy()
+                daugther = daugther or mother.copy()
+
+                # preventing duplicated individuals
+                if new_population.has(son) or new_population.has(daugther):
+                    continue
+
+                new_population.append(son)
+                new_population.append(daugther)
+
+            # if there was no improvement with new population
+            if population.best().fitness() <= new_population.best().fitness():
+                stagnation_count += 1
+            else:
+                stagnation_count = 0  # reset count
+
+            population = new_population
+            generations_count += 1
+
+        self.generations_count = generations_count
+        self.population = population
+
+        return population
+
+
+def genethic(max_stagnation=2, population_size=4):
+    from datetime import datetime
+    help_description = "The program accepts the path of .JSON file as input and computes the path using Genethic algorithm"
+    parserArg = ArgumentParser(description=help_description)
+    parserArg.add_argument("-file", help="TSP File Path", required=True)
+    args = parserArg.parse_args()
+    f_name = args.file
+    if(f_name=="brazil58"):
+        graph = Graph('data/brazil58.json')
+    elif(f_name=="eil101"):
+        graph = Graph('data/eil101.json')
+    elif(f_name=="gil262"):
+        graph = Graph('data/gil262.json')
+    else:
+        print('Escolha um arquivo valido')  
+    solution = Solution('0', graph, max_stagnation=max_stagnation, population_size=population_size)
+    start_time = datetime.now()
+    population = solution.solve()
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    print("Elapsed time:", str(elapsed_time), "ms")
+    print('Generations created:', solution.generations_count)
+    print('Fitness:', population.best().fitness())
+    print('Path:', population.best())
+genethic()
---
+++ Trabalho IA - Greedy/tsp_greedy.py
@@ -0,0 +1,142 @@
+'''
+Developer: Francisco Nascimento, adapted from Abhishek Manoj Sharma
+Course: Artificial Intelligence 2019/2
+Date: August 25, 2019
+'''
+
+from argparse import ArgumentParser
+import time
+import os.path
+from math import sin, cos, sqrt, atan2, radians
+
+# ---------------------------------------------------------------------------------------------------
+# Define the start node of the travel, which should be also the final one
+TOUR_START_NODE_INDEX = 0
+
+# ---------------------------------------------------------------------------------------------------
+# readTSPFile() method to read TSP file and to produce distance matrix with costs
+# ---------------------------------------------------------------------------------------------------
+def readTSPFile(tsp_file_name):
+    number_of_nodes = 0;
+    costs = {}
+    with open(tsp_file_name) as tsp_file:
+        for line in tsp_file:
+            if "DIMENSION" in line:
+                splittedLine = line.strip().split(":")
+                number_of_nodes = int(splittedLine[1])
+            if "EDGE_WEIGHT_TYPE" in line:
+                splittedLine = line.strip().split(":")
+            if "EDGE_WEIGHT_SECTION" in line:
+                i = 0;
+                for line in tsp_file:
+                    if not (line.strip() == "EOF"):
+                        splittedLine = line.strip().split()
+                        for j in range(i+1,len(splittedLine)+i+1):
+                            #costs.append( ( i,j,float(splittedLine[j-i-1]) ) );
+                            costs[i,j] = float(splittedLine[j-i-1]);
+                            #print(i,j,splittedLine[j-i-1]);
+                    i=i+1;
+            if "NODE_COORD_SECTION" in line:
+                i = 0;
+                coordenada_x = []
+                coordenada_y = []
+                #percorre as linhas e joga as coordenadas x e y em duas listas para cada coordenada
+                for line in tsp_file:
+                    if not (line.strip() == "EOF"):
+                        splittedLine = line.strip().split()
+                        coordenada_x.append(float(splittedLine[1]))
+                        coordenada_y.append(float(splittedLine[2]))
+                        #print("x",float(splittedLine[1]),"y",float(splittedLine[2]))
+                i = 0
+                j = 0
+                #Compara cidade com cidade anterior, construindo matriz diagonal superior
+                for i in range(i,len(coordenada_x)):
+                    for j in range(i,len(coordenada_x)):
+                        #calcula distancia com distancia euclidiana
+                        lat1 = coordenada_x[i]
+                        lon1 = coordenada_y[i]
+                        lat2 = coordenada_x[j]
+                        lon2 = coordenada_y[j]
+                        dlon = (lon2 - lon1)**2
+                        dlat = (lat2 - lat1)**2
+                        a = dlat+dlon
+                        #c = 2 * atan2(sqrt(a), sqrt(1 - a))
+                        distance = sqrt(a)
+                        #Ignora distancia da cidade para si mesma
+                        if(i!=j):
+                            costs[i,j] = distance
+                            #print(i,",",j,":",costs[i,j])
+                        #print(i,j,splittedLine[j-i-1]);
+    return number_of_nodes,costs
+
+# ---------------------------------------------------------------------------------------------------
+# greedy_main_run() method drives the program by calling the readTSPFile() method to produce
+#   distance matrix and then the computePathGreedy() method to calculate path using Greedy algorithm
+# ---------------------------------------------------------------------------------------------------
+def greedy_main_run(filename, printTour = False):
+    global TOUR_START_NODE_INDEX
+
+    # read the TSP file provided
+    number_of_nodes,costs = readTSPFile(filename)
+    #print(costs)
+
+    print("------------------------------------------")
+    print("Greedy Algorithm -", filename)
+    start_time = time.time()
+
+    computePathGreedy(number_of_nodes, TOUR_START_NODE_INDEX, costs, printTour)
+
+    print("Total time:", "{0:.3f}".format((time.time() - start_time)), "seconds")
+    print("------------------------------------------")
+
+
+# ---------------------------------------------------------------------------------------------------
+# computePathGreedy() method produces the tour using greedy algorithm
+# ---------------------------------------------------------------------------------------------------
+def computePathGreedy(number_of_nodes, start_node, costs, printTour):
+    global TOUR_START_NODE_INDEX
+    visited_nodes = []
+    total_tour_length = 0.0
+    while (len(visited_nodes) != number_of_nodes-1):
+        visited_nodes.append(start_node)
+        node_distances = {}
+        for key, value in iter(costs.items()):
+            if key[0] == start_node and key[0] != key[1] and key[1] not in visited_nodes:
+                node_distances[key[1]] = value
+            elif key[1] == start_node and key[0] != key[1] and key[0] not in visited_nodes:
+                node_distances[key[0]] = value
+        start_node = min(node_distances, key=node_distances.get)
+        total_tour_length += node_distances.pop(start_node)
+    visited_nodes.append(start_node)
+
+    try:
+        total_tour_length += costs[start_node, TOUR_START_NODE_INDEX]
+    except KeyError:
+        total_tour_length += costs[TOUR_START_NODE_INDEX,start_node]
+
+    if printTour:
+        print("\nPath:", visited_nodes)
+    print("Tour length:", "{0:.3f}".format(total_tour_length) )
+
+# ---------------------------------------------------------------------------------------------------
+# Main program
+# ---------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    help_description = "The program accepts the path of .TSP file as input and computes the path using Greedy algorithm"
+    parser = ArgumentParser(description=help_description)
+    parser.add_argument("-file", help="TSP File Path", required=True)
+    args = parser.parse_args()
+    f_name = args.file
+    if(f_name=="brazil58"):
+        f_name = 'data/brazil58.tsp'
+    elif(f_name=="eil101"):
+        f_name = 'data/eil101.tsp'
+    elif(f_name=="gil262"):
+        f_name = 'data/gil262.tsp'
+    else:
+        print('Escolha um arquivo valido') 
+    if os.path.isfile(f_name):
+        greedy_main_run(f_name, True)
+    else:
+        print(f_name,"- File not found. \nPlease make sure the path is correct and try again.")
+        exit()
